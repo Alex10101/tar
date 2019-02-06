e081cbd882390976243e9fef4433f38d564b415a
Alex10101 <cpt-morgan@mail.ru>
2019-02-06 17:49:04 +0300

    4

--- fs.js
+++ fs.js
@@ -1,13 +1,53 @@
-// head -10 bar.txt
-// tar -tvf 1.tar.gz | head -5 1.txt
-// ls -lh
-
-const util = require('util');
-const fs = require('fs');
 const { exec } = require('child_process');
+let cd = 'cd ./files && exec '
 
-const stat = util.promisify(fs.readdir);
+read = (path, callback) => {
+	if(path == '/favicon.ico') return
+	if(path === undefined) path = '';
+	path = path.slice(1)
 
+	if(path.indexOf('.tar') > -1) {
+		let p = path
+		let tar = p.indexOf('.tar')
 
+		if(p.substr(tar).indexOf('/') > -1) {
+			let head = p.substr(0, tar)
+			let tail = p.substr(tar)
+				tail  = tail.indexOf('/') + head.length
 
-module.exports = stat
+				head = p.substr(0, tail)
+				tail = p.substr(tail + 1)
+				// console.log(head, tail)
+
+			// if file or folder in tar
+			return exec(`${cd} tar -tvf ${head} | head -10 ${tail}`, callback)
+
+		}
+		// if tar
+		return exec(`${cd} tar -tvf ${path}`, callback)
+
+	}
+	if(path.indexOf('.', [1]) > -1) {
+		// if file
+		return exec(`${cd} head -10 ${path}`, callback)
+
+	}
+	// if folder
+	return exec(`${cd}  ls -lh ${path}`, callback)
+
+	
+}
+
+// read(
+// 	// '/1'
+// 	// '/1.txt'
+// 	// '/1.tar.gz'
+// 	'/1.tar.gz/1.txt'
+// 	,
+// 	(err, stdout, stderr) => {
+// 		console.log(`${stdout}`)
+// 		console.log(`${stderr}`)
+// 	}
+// )
+
+module.exports = read
--- server.js
+++ server.js
@@ -1,39 +1,36 @@
-const express = require('express');
+const app = require('express')();
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
-const app = express();
 
-// app.use(express.static('files'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(fileUpload());
 
-const fs = require('./fs.js');
-const path = './files/'
+const { exec } = require('child_process');
+const read = require('./fs.js');
 
-app.get('/', (req, res) => {
-	fs(path).then((data) => {
-		res.send({
-			items : data
-		})
+app.get('*', (req, res) => {
+	read(req.url, (error, stdout, stderr) => {
+	  if (error) {
+	    res.send({
+	    	err : error.toString()
+	    })
+	    return
+	  }
+	  // console.log(`${stdout}`);
+	  res.send({
+		items : stdout
+	  })
 	})
 })
 
-app.get('/:file', (req, res) => {
-	fs(path + req.file).then((data) => {
-		res.send({
-			items : data
-		})
-	})
-})
-
-app.post('/files', (req, res) => {
-	if(!req.files) {
-		res.redirect('/');
+app.post('/', (req, res) => {
+	if(typeof req.body === 'undefined') {
+		return;
 	}
 
-	if(!fs.existsSync(path + req.files.file.name)) {
-		req.files.file.mv(path + req.files.file.name);
+	if(!fs.existsSync(path + req.body.file.name)) {
+		req.body.file.mv(path + req.body.file.name);
  	}
 
  	res.redirect('/');
